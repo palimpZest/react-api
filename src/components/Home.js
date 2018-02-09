8e0a08b77564b35a89b438287a6fbbd0ceb2c95b
palimpZest <polo.vaca365@gmail.com>
2018-02-09 14:03:23 +0100

    fix: Change homepage elements display

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,35 +7,22 @@ class Home extends Component {
   render() {
     return <Content style={{ padding: "0" }}>
         <div className="App">
-          <header className="App-header">
+          <div className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <h2 className="greeting">Hi, there!</h2>
             <h2 className="greeting">Welcome to</h2>
-            <h2 className="App-title">React SWAPI</h2>
-            <br />
-            <br />
-            <p className="welcome-message">
-              This fun little project shows React Router, API calls, and Ant
-              Design in action.
-            </p>
-            <br />
-            <br />
-            <br />
-            <p className="welcome-message">
-              Feel free to browse all the sections!
-            </p>
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-          </header>
+            <h2 className="greeting">React SWAPI</h2>
+            <div className="about">
+              <p className="welcome-message">
+                This fun little project shows React Router, API calls, and
+                Ant Design in action.
+              </p>
+              <p className="welcome-message">
+                Feel free to browse all the sections!
+              </p>
+            </div>
+          </div>
           <img className="warp-img" src="https://media.giphy.com/media/DxRHMazcVPHJS/giphy.gif" alt="warp in space gif" />
-          <br />
-          <br />
-          <div className="back-up-message">Push to go back up!</div>
-          <br />
-          <br />
         </div>
       </Content>;
   }
