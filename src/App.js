468edb5d121bc56d89efc12b254d3f139736e0b6
palimpZest <polo.vaca365@gmail.com>
2020-06-04 12:10:35 +0200

    fix: Menu styles for small screens

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React, { Component } from 'react';
-import { Route, BrowserRouter, Switch, NavLink } from 'react-router-dom';
-import Home from './components/Home';
-import FilmsHolder from './components/FilmsHolder';
-import PeopleHolder from './components/PeopleHolder';
-import SpeciesHolder from './components/SpeciesHolder';
-import VehiclesHolder from './components/VehiclesHolder';
-import StarshipsHolder from './components/StarshipsHolder';
-import PlanetsHolder from './components/PlanetsHolder';
-import PeopleShowItem from './components/routes/PeopleShowItem';
-import PlanetsShowItem from './components/routes/PlanetsShowItem';
-import SpeciesShowItem from './components/routes/SpeciesShowItem';
-import StarshipsShowItem from './components/routes/StarshipsShowItem';
-import VehiclesShowItem from './components/routes/VehiclesShowItem';
-import FilmsShowItem from './components/routes/FilmsShowItem';
-import ScrollToTop from './components/routes/ScrollToTop';
-import NotFound from './components/routes/NotFound';
-import './App.css';
-import { BackTop } from 'antd';
-import arrow from './icons/play-button.svg';
-import { Row } from 'antd';
-import { Layout, Menu } from 'antd';
+import React, { Component } from "react";
+import { Route, BrowserRouter, Switch, NavLink } from "react-router-dom";
+import { BackTop, Layout, Menu } from "antd";
+
+import Home from "./components/Home";
+import FilmsHolder from "./components/FilmsHolder";
+import PeopleHolder from "./components/PeopleHolder";
+import SpeciesHolder from "./components/SpeciesHolder";
+import VehiclesHolder from "./components/VehiclesHolder";
+import StarshipsHolder from "./components/StarshipsHolder";
+import PlanetsHolder from "./components/PlanetsHolder";
+import PeopleShowItem from "./components/routes/PeopleShowItem";
+import PlanetsShowItem from "./components/routes/PlanetsShowItem";
+import SpeciesShowItem from "./components/routes/SpeciesShowItem";
+import StarshipsShowItem from "./components/routes/StarshipsShowItem";
+import VehiclesShowItem from "./components/routes/VehiclesShowItem";
+import FilmsShowItem from "./components/routes/FilmsShowItem";
+import ScrollToTop from "./components/routes/ScrollToTop";
+import NotFound from "./components/routes/NotFound";
+
+import "./App.css";
+
+import arrow from "./icons/play-button.svg";
+
 const { Header } = Layout;
 
 class App extends Component {
@@ -28,56 +30,54 @@ class App extends Component {
       <BrowserRouter>
         <nav>
           <Header className="header-style">
-            <Row
-              type="flex"
-              justify="space-around"
-              gutter={{ xs: 0, sm: 0, md: 0, lg: 4, xl: 8, xxl: 32 }}
+            <Menu
+              theme="dark"
+              mode="horizontal"
+              style={{
+                backgroundColor: "black",
+                color: "#ffd900",
+                display: "flex",
+                justifyContent: "center",
+                flexFlow: "row wrap",
+              }}
             >
-              <Menu
-                theme="dark"
-                mode="horizontal"
-                style={{
-                  backgroundColor: 'black',
-                  color: 'blue'
-                }}
-              >
-                <Menu.Item key="1">
-                  <NavLink exact to="/" activeClassName="selected">
-                    Home
-                  </NavLink>
-                </Menu.Item>
-                <Menu.Item key="2">
-                  <NavLink to="/people" activeClassName="selected">
-                    People
-                  </NavLink>
-                </Menu.Item>
-                <Menu.Item key="3">
-                  <NavLink to="/species" activeClassName="selected">
-                    Species
-                  </NavLink>
-                </Menu.Item>
-                <Menu.Item key="4">
-                  <NavLink to="/vehicles" activeClassName="selected">
-                    Vehicles
-                  </NavLink>
-                </Menu.Item>
-                <Menu.Item key="5">
-                  <NavLink to="/starships" activeClassName="selected">
-                    Starships
-                  </NavLink>
-                </Menu.Item>
-                <Menu.Item key="6">
-                  <NavLink to="/planets" activeClassName="selected">
-                    Planets
-                  </NavLink>
-                </Menu.Item>
-                <Menu.Item key="7">
-                  <NavLink exact to="/films" activeClassName="selected">
-                    Films
-                  </NavLink>
-                </Menu.Item>
-              </Menu>
-            </Row>
+              <Menu.Item key="1">
+                <NavLink exact to="/" activeClassName="selected">
+                  Home
+                </NavLink>
+              </Menu.Item>
+              <Menu.Item key="2">
+                <NavLink to="/people" activeClassName="selected">
+                  People
+                </NavLink>
+              </Menu.Item>
+              <Menu.Item key="3">
+                <NavLink to="/species" activeClassName="selected">
+                  Species
+                </NavLink>
+              </Menu.Item>
+              <Menu.Item key="4">
+                <NavLink to="/vehicles" activeClassName="selected">
+                  Vehicles
+                </NavLink>
+              </Menu.Item>
+              <Menu.Item key="5">
+                <NavLink to="/starships" activeClassName="selected">
+                  Starships
+                </NavLink>
+              </Menu.Item>
+              <Menu.Item key="6">
+                <NavLink to="/planets" activeClassName="selected">
+                  Planets
+                </NavLink>
+              </Menu.Item>
+              <Menu.Item key="7">
+                <NavLink exact to="/films" activeClassName="selected">
+                  Films
+                </NavLink>
+              </Menu.Item>
+            </Menu>
+
             <BackTop>
               <img
                 src={arrow}
